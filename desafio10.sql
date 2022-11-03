9fb6a187ca1ef79a7911216c9d0fc99b202915dc
Luiz Pereira <lfhbpereira@gmail.com>
2022-11-03 13:40:30 -0300

    Update challenge 10

--- desafio10.sql
+++ desafio10.sql
@@ -8,6 +8,8 @@ INNER JOIN SpotifyClone.history AS h
 ON u.user_id = h.user_id
 INNER JOIN SpotifyClone.song AS s
 ON h.song_id = s.song_id
-WHERE p.plan_title = 'gratuito' OR p.plan_title = 'pessoal'
+WHERE
+  p.plan_title = 'gratuito'
+  OR p.plan_title = 'pessoal'
 GROUP BY s.song_title
 ORDER BY s.song_title ASC;
