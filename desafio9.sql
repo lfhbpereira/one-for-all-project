633ad6e428fdd245a08df2ae78c4065a8ba288a9
Luiz Pereira <lfhbpereira@gmail.com>
2022-11-03 12:15:13 -0300

    Add challenge 9 done

---
+++ desafio9.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(h.song_id) AS quantidade_musicas_no_historico
+FROM SpotifyClone.history AS h
+INNER JOIN SpotifyClone.user AS u
+ON h.user_id = u.user_id
+WHERE u.user_name = 'Barbara Liskov';
